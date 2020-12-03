540550749b8f5df12cfea634f2150f4648a46dcd
Diego Santos <desenv.diegosantos@gmail.com>
2020-12-03 00:33:30 -0300

    Files removed by relocation

--- src/containers/Login/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <div>Exemple</div>;
-};
-
-export default Login;
--- src/utils/constants/routers.js
+++
@@ -1,5 +0,0 @@
-export const APP = "/";
-export const TIMELINE = "/timeline";
-export const MY_PROFILE = "/meu-perfil";
-
-export const PRIVATE_ROUTERS = [TIMELINE, MY_PROFILE];
